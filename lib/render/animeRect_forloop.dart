fbeb35b883788a1cecfe2e72b2ab1d5f5ffa7a56
kyorohiro <kyorohiro@gmail.com>
2016-03-16 20:27:49 +0900

    ..

---
+++ lib/render/animeRect_forloop.dart
@@ -0,0 +1,52 @@
+// check 2016 3/16
+import 'package:flutter/widgets.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:async';
+import 'dart:math' as math;
+import 'dart:ui';
+
+void main() {
+  runApp(new DemoWidget()..anime());
+}
+
+class DemoWidget extends SingleChildRenderObjectWidget {
+  double angle = 0.0;
+  DemoRectObject o = new DemoRectObject();
+
+  @override
+  RenderObject createRenderObject(BuildContext context) {
+    return o;
+  }
+
+  Future anime() async {
+    while (true) {
+      await new Future.delayed(new Duration(milliseconds: 20));
+      o.x = 100 * math.cos(math.PI * angle / 180.0) + 100.0;
+      o.y = 100 * math.sin(math.PI * angle / 180.0) + 100.0;
+      angle++;
+      o.markNeedsPaint();
+    }
+  }
+}
+
+class DemoRectObject extends RenderConstrainedBox {
+  double x = 50.0;
+  double y = 50.0;
+  DemoRectObject() : super(additionalConstraints: const BoxConstraints.expand()) {
+    ;
+  }
+
+  @override
+  bool hitTestSelf(Point position) => true;
+
+  @override
+  void handleEvent(PointerEvent event, BoxHitTestEntry entry) {}
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    Paint p = new Paint();
+    p.color = new Color.fromARGB(0xff, 0xff, 0xff, 0xff);
+    Rect r = new Rect.fromLTWH(x, y, 25.0, 25.0);
+    context.canvas.drawRect(r, p);
+  }
+}
